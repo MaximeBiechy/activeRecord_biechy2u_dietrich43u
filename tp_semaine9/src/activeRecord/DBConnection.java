57ab6e210b21d7d3052682ab6b6ea7208bfd4403
Maxime Biechy <maxime.bch12@gmail.com>
2023-11-22 15:07:57 +0100

    classe DBConnection

---
+++ tp_semaine9/src/activeRecord/DBConnection.java
@@ -0,0 +1,15 @@
+package activeRecord;
+
+public class DBConnection {
+
+  private static DBConnection instance;
+
+  private DBConnection(){
+
+  }
+
+  public static synchronized DBConnection getInstance(){
+    if (instance == null) instance = new DBConnection();
+    return instance;
+  }
+}
