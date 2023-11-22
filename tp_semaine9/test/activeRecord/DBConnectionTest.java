2910274e63557dc51349d3537247d111f59c651f
Maxime Biechy <maxime.bch12@gmail.com>
2023-11-22 15:17:21 +0100

    test classe DBConnection

---
+++ tp_semaine9/test/activeRecord/DBConnectionTest.java
@@ -0,0 +1,19 @@
+package activeRecord;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DBConnectionTest {
+
+  @Test
+  void testGetConnection() throws SQLException {
+    Connection c1 = DBConnection.getInstance().getConnection();
+    Connection c2 = DBConnection.getInstance().getConnection();
+
+    assertEquals(c1, c2, "ça devrait être la même connection !");
+  }
+}
