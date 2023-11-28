e013bbcf8fc33bc6f527eef21a4e9872dbfb601f
CocoDietrich <corentindietrich57@gmail.com>
2023-11-28 09:27:52 +0100

    getters et setters de Personne

--- tp_semaine9/src/activeRecord/Personne.java
+++ tp_semaine9/src/activeRecord/Personne.java
@@ -5,6 +5,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Personne {
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getNom() {
+    return nom;
+  }
+
+  public void setNom(String nom) {
+    this.nom = nom;
+  }
+
+  public String getPrenom() {
+    return prenom;
+  }
+
+  public void setPrenom(String prenom) {
+    this.prenom = prenom;
+  }
 
   private int id;
   private String nom, prenom;
